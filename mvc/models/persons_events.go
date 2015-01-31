c4c082275bc8ec90cd67ab1da5d9d507cfffc0a1
GuraYaroslava <yaroslava_05@mail.ru>
2015-02-01 01:58:41 +1000

    persons_events: fix whitespace

--- mvc/models/persons_events.go
+++ mvc/models/persons_events.go
@@ -1,47 +1,69 @@
 package models
 
 import (
-	"github.com/orc/db"
+    "github.com/orc/db"
 )
 
 func (c *ModelManager) PersonsEvents() *PersonsEventsModel {
-	model := new(PersonsEventsModel)
+    model := new(PersonsEventsModel)
 
-	model.TableName = "persons_events"
-	model.Caption = "Персоны - Мероприятия"
+    model.TableName = "persons_events"
+    model.Caption = "Персоны - Мероприятия"
 
-	model.Columns = []string{"id", "person_id", "event_id", "reg_date", "last_date"}
-	model.ColNames = []string{"ID", "Персона", "Мероприятие", "Дата регистрации", "Дата последних изменений"}
+    model.Columns = []string{"id", "person_id", "event_id", "reg_date", "last_date"}
+    model.ColNames = []string{"ID", "Персона", "Мероприятие", "Дата регистрации", "Дата последних изменений"}
 
-	tmp := map[string]*Field{
-		"id":        {"id", "ID", "int", false},
-		"person_id": {"person_id", "Персона", "int", true},
-		"event_id":  {"event_id", "Мероприятие", "int", true},
-		"reg_date":  {"reg_date", "Дата регистрации", "date", true},
-		"last_date": {"last_date", "Дата последних изменений", "date", true},
-	}
+    model.Fields = []map[string]string{
+        {
+            "field": "id",
+            "type":  "int",
+            "null":  "NOT NULL",
+            "extra": "PRIMARY"},
+        {
+            "field":    "person_id",
+            "type":     "int",
+            "null":     "NOT NULL",
+            "extra":    "REFERENCES",
+            "refTable": "persons",
+            "refField": "id"},
+        {
+            "field":    "event_id",
+            "type":     "int",
+            "null":     "NOT NULL",
+            "extra":    "REFERENCES",
+            "refTable": "events",
+            "refField": "id"},
+        {
+            "field": "reg_date",
+            "type":  "date",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field": "last_date",
+            "type":  "date",
+            "null":  "NOT NULL",
+            "extra": ""},
+    }
 
-	model.Fields = tmp
+    model.Ref = true
+    model.RefFields = []string{"name", "fname"}
+    model.RefData = make(map[string]interface{}, 2)
 
-	model.Ref = true
-	model.RefFields = []string{"name", "fname"}
-	model.RefData = make(map[string]interface{}, 2)
+    result := db.Select("persons", nil, "", []string{"id", "fname"})
+    model.RefData["person_id"] = make([]interface{}, len(result))
+    model.RefData["person_id"] = result
 
-	result := db.Select("persons", nil, "", []string{"id", "fname"})
-	model.RefData["person_id"] = make([]interface{}, len(result))
-	model.RefData["person_id"] = result
+    result = db.Select("events", nil, "", []string{"id", "name"})
+    model.RefData["event_id"] = make([]interface{}, len(result))
+    model.RefData["event_id"] = result
 
-	result = db.Select("events", nil, "", []string{"id", "name"})
-	model.RefData["event_id"] = make([]interface{}, len(result))
-	model.RefData["event_id"] = result
+    model.Sub = false
+    model.SubTable = nil
+    model.SubField = ""
 
-	model.Sub = false
-	model.SubTable = nil
-	model.SubField = ""
-
-	return model
+    return model
 }
 
 type PersonsEventsModel struct {
-	Entity
+    Entity
 }
