334a028ac195b989007603417f59b2b3b1e0a332
GuraYaroslava <yaroslava_05@mail.ru>
2015-02-01 01:59:33 +1000

    users: fix whitespace

--- mvc/models/users.go
+++ mvc/models/users.go
@@ -3,40 +3,64 @@ package models
 import "github.com/orc/db"
 
 func (c *ModelManager) Users() *UsersModel {
-	model := new(UsersModel)
+    model := new(UsersModel)
 
-	model.TableName = "users"
-	model.Caption = "Пользователи"
+    model.TableName = "users"
+    model.Caption = "Пользователи"
 
-	model.Columns = []string{"id", "login", "role", "person_id"}
-	model.ColNames = []string{"ID", "Логин", "Роль", "Персона"}
+    model.Columns = []string{"id", "login", "role", "person_id"}
+    model.ColNames = []string{"ID", "Логин", "Роль", "Персона"}
 
-	tmp := map[string]*Field{
-		"id":        {"id", "ID", "int", false},
-		"login":     {"login", "Логин", "text", false},
-		"pass":      {"password", "Хеш", "text", false},
-		"salt":      {"salt", "Соль", "text", false},
-		"role":      {"role", "Роль", "text", false},
-		"person_id": {"person_id", "Персона", "text", true},
-	}
+    model.Fields = []map[string]string{
+        {
+            "field": "id",
+            "type":  "int",
+            "null":  "NOT NULL",
+            "extra": "PRIMARY"},
+        {
+            "field": "login",
+            "type":  "text",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field": "pass",
+            "type":  "text",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field": "salt",
+            "type":  "text",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field": "role",
+            "type":  "text",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field":    "person_id",
+            "type":     "int",
+            "null":     "NOT NULL",
+            "extra":    "REFERENCES",
+            "refTable": "persons",
+            "refField": "id"},
+    }
 
-	model.Fields = tmp
+    model.Ref = true
+    model.RefFields = []string{"fname"}
+    model.RefData = make(map[string]interface{}, 1)
 
-	model.Ref = true
-	model.RefFields = []string{"fname"}
-	model.RefData = make(map[string]interface{}, 1)
+    result := db.Select("persons", nil, "", []string{"id", "fname"})
+    model.RefData["person_id"] = make([]interface{}, len(result))
+    model.RefData["person_id"] = result
 
-	result := db.Select("persons", nil, "", []string{"id", "fname"})
-	model.RefData["person_id"] = make([]interface{}, len(result))
-	model.RefData["person_id"] = result
+    model.Sub = false
+    model.SubTable = nil
+    model.SubField = ""
 
-	model.Sub = false
-	model.SubTable = nil
-	model.SubField = ""
-
-	return model
+    return model
 }
 
 type UsersModel struct {
-	Entity
+    Entity
 }
