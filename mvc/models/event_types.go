81aea7d52239ccc2a01e1e82ffcd29d142c80f50
GuraYaroslava <yaroslava_05@mail.ru>
2015-02-01 01:50:39 +1000

    event_types: fix whitespace

--- mvc/models/event_types.go
+++ mvc/models/event_types.go
@@ -1,34 +1,48 @@
 package models
 
 func (c *ModelManager) EventTypes() *EventTypesModel {
-	model := new(EventTypesModel)
+    model := new(EventTypesModel)
 
-	model.TableName = "event_types"
-	model.Caption = "Типы мероприятий"
+    model.TableName = "event_types"
+    model.Caption = "Типы мероприятий"
 
-	model.Columns = []string{"id", "name", "description", "topicality"}
-	model.ColNames = []string{"ID", "Тип", "Описание", "Актуальность"}
+    model.Columns = []string{"id", "name", "description", "topicality"}
+    model.ColNames = []string{"ID", "Тип", "Описание", "Актуальность"}
 
-	tmp := map[string]*Field{
-		"id":          {"id", "ID", "int", false},
-		"name":        {"name", "Тип", "text", false},
-		"description": {"description", "Описание", "text", false},
-		"topicality":  {"topicality", "Актуальность", "boolean", false},
-	}
+    model.Fields = []map[string]string {
+        {
+            "field": "id",
+            "type":  "int",
+            "null":  "NOT NULL",
+            "extra": "PRIMARY"},
+        {
+            "field": "name",
+            "type":  "text",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field": "description",
+            "type":  "text",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field": "topicality",
+            "type":  "boolean",
+            "null":  "NOT NULL",
+            "extra": ""},
+    }
 
-	model.Fields = tmp
+    model.Ref = false
+    model.RefData = nil
+    model.RefFields = nil
 
-	model.Ref = false
-	model.RefData = nil
-	model.RefFields = nil
+    model.Sub = true
+    model.SubTable = []string{"forms_types", "events_types"}
+    model.SubField = "type_id"
 
-	model.Sub = true
-	model.SubTable = []string{"forms_types", "events_types"}
-	model.SubField = "type_id"
-
-	return model
+    return model
 }
 
 type EventTypesModel struct {
-	Entity
+    Entity
 }
