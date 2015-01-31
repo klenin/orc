a6302c844af07631c5fc3f07d1a58931a2003075
GuraYaroslava <yaroslava_05@mail.ru>
2015-02-01 02:00:32 +1000

    hide teams

--- mvc/models/teams.go
+++ mvc/models/teams.go
@@ -1,32 +1,33 @@
 package models
 
-func (c *ModelManager) Teams() *TeamsModel {
-	model := new(TeamsModel)
+/*func (c *ModelManager) Teams() *TeamsModel {
+    model := new(TeamsModel)
 
-	model.TableName = "teams"
-	model.Caption = "Команды"
+    model.TableName = "teams"
+    model.Caption = "Команды"
 
-	model.Columns = []string{"id", "name"}
-	model.ColNames = []string{"ID", "Название"}
+    model.Columns = []string{"id", "name"}
+    model.ColNames = []string{"ID", "Название"}
 
-	tmp := map[string]*Field{
-		"id":   {"id", "ID", "int", false},
-		"name": {"fname", "Название", "text", false},
-	}
+    tmp := map[string]*Field{
+        "id":   {"id", "ID", "int", false},
+        "name": {"fname", "Название", "text", false},
+    }
 
-	model.Fields = tmp
+    model.Fields = tmp
 
-	model.Ref = false
-	model.RefData = nil
-	model.RefFields = nil
+    model.Ref = false
+    model.RefData = nil
+    model.RefFields = nil
 
-	model.Sub = true
-	model.SubTable = []string{"teams_persons"}
-	model.SubField = "team_id"
+    model.Sub = true
+    model.SubTable = []string{"teams_persons"}
+    model.SubField = "team_id"
 
-	return model
+    return model
 }
 
 type TeamsModel struct {
-	Entity
+    Entity
 }
+*/
