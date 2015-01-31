36da1bd4d6ff0ec02398926059ad04783517b38e
GuraYaroslava <yaroslava_05@mail.ru>
2015-02-01 01:54:17 +1000

    params: fix whitespace

--- mvc/models/params.go
+++ mvc/models/params.go
@@ -1,53 +1,73 @@
 package models
 
 import (
-	"github.com/orc/db"
+    "github.com/orc/db"
 )
 
 func (c *ModelManager) Params() *ParamsModel {
-	model := new(ParamsModel)
+    model := new(ParamsModel)
 
-	model.TableName = "params"
-	model.Caption = "Параметры"
+    model.TableName = "params"
+    model.Caption = "Параметры"
 
-	model.Columns = []string{"id", "name", "type", "form_id", "identifier"}
-	model.ColNames = []string{"ID", "Название", "Тип", "Форма", "Идентификатор"}
+    model.Columns = []string{"id", "name", "type", "form_id", "identifier"}
+    model.ColNames = []string{"ID", "Название", "Тип", "Форма", "Идентификатор"}
 
-	tmp := map[string]*Field{
-		"id":         {"id", "ID", "int", false},
-		"name":       {"name", "Название", "text", false},
-		"type":       {"type", "Тип", "text", false},
-		"form_id":    {"form_id", "Форма", "int", true},
-		"identifier": {"identifier", "Идентификатор", "text", true},
-	}
+    model.Fields = []map[string]string{
+        {
+            "field": "id",
+            "type":  "int",
+            "null":  "NOT NULL",
+            "extra": "PRIMARY"},
+        {
+            "field": "name",
+            "type":  "text",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field": "type",
+            "type":  "text",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field":    "form_id",
+            "type":     "int",
+            "null":     "NOT NULL",
+            "extra":    "REFERENCES",
+            "refTable": "forms",
+            "refField": "id"},
+        {
+            "field": "identifier",
+            "type":  "text",
+            "null":  "NOT NULL",
+            "extra": ""},
+    }
 
-	model.Fields = tmp
+    model.Ref = true
+    model.RefFields = []string{"name"}
+    model.RefData = make(map[string]interface{}, 1)
 
-	model.Ref = true
-	model.RefFields = []string{"name"}
-	model.RefData = make(map[string]interface{}, 1)
+    result := db.Select("forms", nil, "", []string{"id", "name"})
+    model.RefData["form_id"] = make([]interface{}, len(result))
+    model.RefData["form_id"] = result
 
-	result := db.Select("forms", nil, "", []string{"id", "name"})
-	model.RefData["form_id"] = make([]interface{}, len(result))
-	model.RefData["form_id"] = result
+    model.RefData["type"] = []interface{}{
+        map[string]string{"id": "0", "name": "date"},
+        map[string]string{"id": "1", "name": "region"},
+        map[string]string{"id": "2", "name": "district"},
+        map[string]string{"id": "3", "name": "city"},
+        map[string]string{"id": "4", "name": "street"},
+        map[string]string{"id": "5", "name": "building"},
+        map[string]string{"id": "6", "name": "input"},
+        map[string]string{"id": "7", "name": "textarea"}}
 
-	model.RefData["type"] = []interface{}{
-		map[string]string{"id": "0", "name": "date"},
-		map[string]string{"id": "1", "name": "region"},
-		map[string]string{"id": "2", "name": "district"},
-		map[string]string{"id": "3", "name": "city"},
-		map[string]string{"id": "4", "name": "street"},
-		map[string]string{"id": "5", "name": "building"},
-		map[string]string{"id": "6", "name": "input"},
-		map[string]string{"id": "7", "name": "textarea"}}
+    model.Sub = false
+    model.SubTable = nil
+    model.SubField = ""
 
-	model.Sub = false
-	model.SubTable = nil
-	model.SubField = ""
-
-	return model
+    return model
 }
 
 type ParamsModel struct {
-	Entity
+    Entity
 }
