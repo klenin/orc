969c9455ea9f203c241c8089bc031d91f5a30ac3
GuraYaroslava <yaroslava_05@mail.ru>
2015-02-01 01:52:32 +1000

    form_types: fix whitespace

--- mvc/models/forms_types.go
+++ mvc/models/forms_types.go
@@ -1,46 +1,59 @@
 package models
 
 import (
-	"github.com/orc/db"
+    "github.com/orc/db"
 )
 
 func (c *ModelManager) FormsTypes() *FormsTypesModel {
-	model := new(FormsTypesModel)
+    model := new(FormsTypesModel)
 
-	model.TableName = "forms_types"
-	model.Caption = "Формы - Типы мероприятий"
+    model.TableName = "forms_types"
+    model.Caption = "Формы - Типы мероприятий"
 
-	model.Columns = []string{"id", "form_id", "type_id", "serial_number"}
-	model.ColNames = []string{"ID", "Форма", "Тип", "Порядковый номер"}
+    model.Columns = []string{"id", "form_id", "type_id", "serial_number"}
+    model.ColNames = []string{"ID", "Форма", "Тип", "Порядковый номер"}
 
-	tmp := map[string]*Field{
-		"id":            {"id", "ID", "int", false},
-		"form_id":       {"form_id", "Форма", "int", true},
-		"type_id":       {"type_id", "Тип", "int", true},
-		"serial_number": {"serial_number", "Порядковый номер", "int", false},
-	}
+    model.Fields = []map[string]string{
+        {
+            "field": "id",
+            "type":  "int",
+            "null":  "NOT NULL",
+            "extra": "PRIMARY"},
+        {
+            "field":    "form_id",
+            "type":     "int",
+            "null":     "NOT NULL",
+            "extra":    "REFERENCES",
+            "refTable": "forms",
+            "refField": "id"},
+        {
+            "field":    "type_id",
+            "type":     "int",
+            "null":     "NOT NULL",
+            "extra":    "REFERENCES",
+            "refTable": "event_types",
+            "refField": "id"},
+    }
 
-	model.Fields = tmp
+    model.Ref = true
+    model.RefFields = []string{"name"}
+    model.RefData = make(map[string]interface{}, 2)
 
-	model.Ref = true
-	model.RefFields = []string{"name"}
-	model.RefData = make(map[string]interface{}, 2)
+    result := db.Select("forms", nil, "", []string{"id", "name"})
+    model.RefData["form_id"] = make([]interface{}, len(result))
+    model.RefData["form_id"] = result
 
-	result := db.Select("forms", nil, "", []string{"id", "name"})
-	model.RefData["form_id"] = make([]interface{}, len(result))
-	model.RefData["form_id"] = result
+    result = db.Select("event_types", nil, "", []string{"id", "name"})
+    model.RefData["type_id"] = make([]interface{}, len(result))
+    model.RefData["type_id"] = result
 
-	result = db.Select("event_types", nil, "", []string{"id", "name"})
-	model.RefData["type_id"] = make([]interface{}, len(result))
-	model.RefData["type_id"] = result
+    model.Sub = false
+    model.SubTable = nil
+    model.SubField = ""
 
-	model.Sub = false
-	model.SubTable = nil
-	model.SubField = ""
-
-	return model
+    return model
 }
 
 type FormsTypesModel struct {
-	Entity
+    Entity
 }
