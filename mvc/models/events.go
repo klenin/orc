474fcbad768253ca464c6b1384ed85d2e0e9e8e8
GuraYaroslava <yaroslava_05@mail.ru>
2015-02-01 01:42:00 +1000

    events: fix model.Fields format data, fix whitespace

--- mvc/models/events.go
+++ mvc/models/events.go
@@ -1,35 +1,57 @@
 package models
 
 func (c *ModelManager) Events() *EventModel {
-	model := new(EventModel)
+    model := new(EventModel)
 
-	model.TableName = "events"
-	model.Caption = "Мероприятия"
+    model.TableName = "events"
+    model.Caption = "Мероприятия"
 
-	model.Columns = []string{"id", "name", "date_start", "date_end", "time", "url"}
-	model.ColNames = []string{"ID", "Название", "Дата начала", "Дата окончания", "Время", "Сайт"}
+    model.Columns = []string{"id", "name", "date_start", "date_finish", "time", "url"}
+    model.ColNames = []string{"ID", "Название", "Дата начала", "Дата окончания", "Время", "Сайт"}
 
-	tmp := map[string]*Field{
-		"id":         {"id", "ID", "int", false},
-		"name":       {"name", "Название", "text", false},
-		"date_start": {"date", "Дата начала", "date", false},
-		"date_end":   {"date", "Дата окончания", "date", false},
-		"time":       {"time", "Время", "time", false},
-		"url":        {"url", "Сайт", "text", false},
-	}
+    model.Fields = []map[string]string{
+        {
+            "field": "id",
+            "type":  "int",
+            "null":  "NOT NULL",
+            "extra": "PRIMARY"},
+        {
+            "field": "name",
+            "type":  "text",
+            "null":  "NOT NULL",
+            "extra": "UNIQUE"},
+        {
+            "field": "date_start",
+            "type":  "date",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field": "date_finish",
+            "type":  "date",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field": "time",
+            "type":  "time",
+            "null":  "NOT NULL",
+            "extra": ""},
+        {
+            "field": "url",
+            "type":  "text",
+            "null":  "NOT NULL",
+            "extra": ""},
+    }
 
-	model.Fields = tmp
+    model.Ref = false
+    model.RefData = nil
+    model.RefFields = nil
 
-	model.Ref = false
-	model.RefData = nil
-	model.RefFields = nil
-
-	model.Sub = true
-	model.SubTable = []string{"events_types"}
-	model.SubField = "event_id"
-	return model
+    model.Sub = true
+    model.SubTable = []string{"events_types"}
+    model.SubField = "event_id"
+    return model
 }
 
 type EventModel struct {
-	Entity
+    Entity
 }
