3c08619a367a20291f8af09018a918604fc14a92
GuraYaroslava <yaroslava_05@mail.ru>
2015-02-01 16:53:10 +1000

    utils: fix whitespace, print error to log

--- utils/utils.go
+++ utils/utils.go
@@ -1,41 +1,39 @@
 package utils
 
 import (
-	"fmt"
-	"net/http"
-	"os"
-	"reflect"
-	"strconv"
+    "fmt"
+    "log"
+    "net/http"
+    "os"
+    "reflect"
+    "strconv"
 )
 
 func HandleErr(message string, err error, w http.ResponseWriter) {
-	if err != nil {
-		println(err.Error())
-		fmt.Printf(message+"%v\n", err.Error())
-		if w != nil {
-			http.Error(w, fmt.Sprintf(message+"%v\n", err.Error()), http.StatusMethodNotAllowed)
-		} else {
-			os.Exit(1)
-		}
-	}
+    if err != nil {
+        log.Println(err.Error())
+        if w != nil {
+            http.Error(w, fmt.Sprintf(message+"%v\n", err.Error()), http.StatusMethodNotAllowed)
+        } else {
+            os.Exit(1)
+        }
+    }
 }
 
 func ArrayInterfaceToString(array []interface{}) []string {
-	s := reflect.ValueOf(array)
-
-	result := make([]string, s.Len())
-	for i, v := range array {
-		switch v.(type) {
-		case int:
-			result[i] = strconv.Itoa(v.(int))
-		case int64:
-			result[i] = strconv.Itoa(int(v.(int64)))
-		case float64:
-			result[i] = strconv.Itoa(int(v.(float64)))
-		case string:
-			result[i] = v.(string)
-		}
-
-	}
-	return result
+    s := reflect.ValueOf(array)
+    result := make([]string, s.Len())
+    for i, v := range array {
+        switch v.(type) {
+        case int:
+            result[i] = strconv.Itoa(v.(int))
+        case int64:
+            result[i] = strconv.Itoa(int(v.(int64)))
+        case float64:
+            result[i] = strconv.Itoa(int(v.(float64)))
+        case string:
+            result[i] = v.(string)
+        }
+    }
+    return result
 }
