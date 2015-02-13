f436f8e0736b0709820de69c44d28f05799a3783
GuraYaroslava <yaroslava_05@mail.ru>
2015-02-14 00:33:51 +1000

    scheme.go: merge two cycles

--- scheme/scheme.go
+++ scheme/scheme.go
@@ -7,15 +7,9 @@ import (
 )
 
 func Init() {
-    Drop()
-    for i, _ := range db.Tables {
-        controllers.GetModel(db.Tables[i]).Create()
-    }
-}
-
-func Drop() {
-    for _, v := range db.Tables {
+    for i, v := range db.Tables {
         db.Query(fmt.Sprintf("DROP TABLE IF EXISTS %s CASCADE;", v), nil)
         db.Query(fmt.Sprintf("DROP SEQUENCE IF EXISTS %s_id_seq;", v), nil)
+        controllers.GetModel(db.Tables[i]).Create()
     }
 }
