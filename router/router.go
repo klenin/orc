9498aa32916ec24c23b0d666ec378735fc347ec3
GuraYaroslava <yaroslava_05@mail.ru>
2015-02-16 04:42:50 +1000

    router: fix indent

--- router/router.go
+++ router/router.go
@@ -1,90 +1,90 @@
 package router
 
 import (
-	"github.com/orc/mvc/controllers"
-	"net/http"
-	"reflect"
-	"strings"
+    "github.com/orc/mvc/controllers"
+    "net/http"
+    "reflect"
+    "strings"
 )
 
 type FastCGIServer struct{}
 
 func (this FastCGIServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	url := r.URL
-	parts := strings.Split(url.Path, "/")
-	controllerName := "index"
-	methodName := "index"
+    url := r.URL
+    parts := strings.Split(url.Path, "/")
+    controllerName := "index"
+    methodName := "index"
 
-	if len(parts) < 2 {
-		//index
-	} else if len(parts) < 3 {
-		if parts[1] != "" {
-			controllerName = parts[1]
-		}
-	} else {
-		controllerName = parts[1]
-		if parts[2] != "" {
-			methodName = parts[2]
-		}
-	}
-	controller := FindController(controllerName)
-	if controller != nil {
-		controller.Elem().FieldByName("Request").Set(reflect.ValueOf(r))
-		controller.Elem().FieldByName("Response").Set(reflect.ValueOf(w))
-		cType := controller.Type()
-		cMethod := FindMethod(cType, methodName)
-		if cMethod != nil {
-			params := PopulateParams(*cMethod, parts)
-			allParams := make([]reflect.Value, len(params)+1)
-			allParams[0] = *controller
-			for i := 0; i < len(params); i++ {
-				allParams[i+1] = params[i]
-			}
-			cMethod.Func.Call(allParams)
-		} else {
-			http.Error(w, "Unable to locate index method in controller.", http.StatusMethodNotAllowed)
-		}
-	} else {
-		http.Error(w, "Unable to locate default controller.", http.StatusMethodNotAllowed)
-	}
+    if len(parts) < 2 {
+        //index
+    } else if len(parts) < 3 {
+        if parts[1] != "" {
+            controllerName = parts[1]
+        }
+    } else {
+        controllerName = parts[1]
+        if parts[2] != "" {
+            methodName = parts[2]
+        }
+    }
+    controller := FindController(controllerName)
+    if controller != nil {
+        controller.Elem().FieldByName("Request").Set(reflect.ValueOf(r))
+        controller.Elem().FieldByName("Response").Set(reflect.ValueOf(w))
+        cType := controller.Type()
+        cMethod := FindMethod(cType, methodName)
+        if cMethod != nil {
+            params := PopulateParams(*cMethod, parts)
+            allParams := make([]reflect.Value, len(params)+1)
+            allParams[0] = *controller
+            for i := 0; i < len(params); i++ {
+                allParams[i+1] = params[i]
+            }
+            cMethod.Func.Call(allParams)
+        } else {
+            http.Error(w, "Unable to locate index method in controller.", http.StatusMethodNotAllowed)
+        }
+    } else {
+        http.Error(w, "Unable to locate default controller.", http.StatusMethodNotAllowed)
+    }
 }
 
 func FindController(controllerName string) *reflect.Value {
-	baseController := new(controllers.BaseController)
-	cmt := reflect.TypeOf(baseController)
-	count := cmt.NumMethod()
-	for i := 0; i < count; i++ {
-		cmt_method := cmt.Method(i)
-		if strings.ToLower(cmt_method.Name) == strings.ToLower(controllerName) {
-			params := make([]reflect.Value, 1)
-			params[0] = reflect.ValueOf(baseController)
-			result := cmt_method.Func.Call(params)
-			return &result[0]
-		}
-	}
-	return nil
+    baseController := new(controllers.BaseController)
+    cmt := reflect.TypeOf(baseController)
+    count := cmt.NumMethod()
+    for i := 0; i < count; i++ {
+        cmt_method := cmt.Method(i)
+        if strings.ToLower(cmt_method.Name) == strings.ToLower(controllerName) {
+            params := make([]reflect.Value, 1)
+            params[0] = reflect.ValueOf(baseController)
+            result := cmt_method.Func.Call(params)
+            return &result[0]
+        }
+    }
+    return nil
 }
 
 func FindMethod(cType reflect.Type, methodName string) *reflect.Method {
-	count := cType.NumMethod()
-	for i := 0; i < count; i++ {
-		method := cType.Method(i)
-		if strings.ToLower(method.Name) == strings.ToLower(methodName) {
-			return &method
-		}
-	}
-	return nil
+    count := cType.NumMethod()
+    for i := 0; i < count; i++ {
+        method := cType.Method(i)
+        if strings.ToLower(method.Name) == strings.ToLower(methodName) {
+            return &method
+        }
+    }
+    return nil
 }
 
 func PopulateParams(method reflect.Method, parts []string) []reflect.Value {
-	numParams := method.Type.NumIn() - 1
-	params := make([]reflect.Value, numParams)
-	for x := 0; x < numParams; x++ {
-		if len(parts) > (x + 3) {
-			params[x] = reflect.ValueOf(parts[x+3])
-		} else {
-			params[x] = reflect.ValueOf("")
-		}
-	}
-	return params
+    numParams := method.Type.NumIn() - 1
+    params := make([]reflect.Value, numParams)
+    for x := 0; x < numParams; x++ {
+        if len(parts) > (x + 3) {
+            params[x] = reflect.ValueOf(parts[x+3])
+        } else {
+            params[x] = reflect.ValueOf("")
+        }
+    }
+    return params
 }
