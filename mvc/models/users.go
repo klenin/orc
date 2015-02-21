c6b7ed75bf99aecb3928a3b8cc4929e09586ba84
GuraYaroslava <yaroslava_05@mail.ru>
2015-02-22 01:34:31 +1000

    user's model: fix fields

--- mvc/models/users.go
+++ mvc/models/users.go
@@ -1,13 +1,12 @@
 package models
 
 type User struct {
-    Id       int    `name:"id" type:"int" null:"NOT NULL" extra:"PRIMARY"`
-    Login    string `name:"login" type:"text" null:"NOT NULL" extra:""`
-    Pass     string `name:"pass" type:"text" null:"NOT NULL" extra:""`
-    Salt     string `name:"salt" type:"text" null:"NOT NULL" extra:""`
-    Role     string `name:"role" type:"text" null:"NOT NULL" extra:""`
-    Hash     string `name:"hash" type:"text" null:"NULL" extra:""`
-    PersonId int    `name:"person_id" type:"int" null:"NOT NULL" extra:"REFERENCES" refTable:"persons" refField:"id" refFieldShow:"fname"`
+    Id    int    `name:"id" type:"int" null:"NOT NULL" extra:"PRIMARY"`
+    Login string `name:"login" type:"text" null:"NOT NULL" extra:""`
+    Pass  string `name:"pass" type:"text" null:"NOT NULL" extra:""`
+    Salt  string `name:"salt" type:"text" null:"NOT NULL" extra:""`
+    Role  string `name:"role" type:"text" null:"NOT NULL" extra:""`
+    Hash  string `name:"hash" type:"text" null:"NULL" extra:""`
 }
 
 func (c *ModelManager) Users() *UsersModel {
@@ -16,8 +15,8 @@ func (c *ModelManager) Users() *UsersModel {
     model.TableName = "users"
     model.Caption = "Пользователи"
 
-    model.Columns = []string{"id", "login", "role", "person_id"}
-    model.ColNames = []string{"ID", "Логин", "Роль", "Персона"}
+    model.Columns = []string{"id", "login", "role"}
+    model.ColNames = []string{"ID", "Логин", "Роль"}
 
     model.Fields = new(User)
     model.WherePart = make(map[string]interface{}, 0)
