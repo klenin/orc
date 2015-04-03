899cde30a3de9094f2b9fd35864e93b9d70c7947
GuraYaroslava <yaroslava_05@mail.ru>
2015-04-03 15:55:06 +1000

    add contest

--- mvc/controllers/index.go
+++ mvc/controllers/index.go
@@ -275,5 +275,4 @@ func CreateRegistrationEvent() {
     //     "form_id":       form_id6,
     //     "param_type_id": param_text_type_id,
     //     "identifier":    16})
-    db.QueryInsert_(params, "RETURNING id")
 }
