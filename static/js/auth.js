07cddda6168b6a70116625bc2bf718636fd8444a
GuraYaroslava <yaroslava_05@mail.ru>
2015-04-06 01:55:06 +1000

    fix error: add right bracket

--- static/js/auth.js
+++ static/js/auth.js
@@ -13,7 +13,7 @@ function(utils) {
 
         } else if (data.result === "badPassword") {
             $("#server-answer").text("Неверный пароль.").css("color", "red");
-        } else if data.result === "notEnabled") {
+        } else if (data.result === "notEnabled") {
             $("#server-answer").text("Ваш аккаунт заблокирован.").css("color", "red");
         }
     };
