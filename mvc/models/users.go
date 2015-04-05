ae5cf84a1ca1927778c72fb19cc140dfa67b0013
GuraYaroslava <yaroslava_05@mail.ru>
2015-04-05 17:00:40 +1000

    model of add fields `token` and `enable` to model of user

--- mvc/models/users.go
+++ mvc/models/users.go
@@ -1,12 +1,14 @@
 package models
 
 type User struct {
-    Id    int    `name:"id" type:"int" null:"NOT NULL" extra:"PRIMARY"`
-    Login string `name:"login" type:"text" null:"NOT NULL" extra:""`
-    Pass  string `name:"pass" type:"text" null:"NOT NULL" extra:""`
-    Salt  string `name:"salt" type:"text" null:"NOT NULL" extra:""`
-    Role  string `name:"role" type:"text" null:"NOT NULL" extra:""`
-    Hash  string `name:"hash" type:"text" null:"NULL" extra:""`
+    Id      int    `name:"id" type:"int" null:"NOT NULL" extra:"PRIMARY"`
+    Login   string `name:"login" type:"text" null:"NOT NULL" extra:""`
+    Pass    string `name:"pass" type:"text" null:"NOT NULL" extra:""`
+    Salt    string `name:"salt" type:"text" null:"NOT NULL" extra:""`
+    Role    string `name:"role" type:"text" null:"NOT NULL" extra:""`
+    Hash    string `name:"hash" type:"text" null:"NULL" extra:""`
+    Token   string `name:"token" type:"text" null:"NULL" extra:""`
+    Enabled bool   `name:"enabled" type:"boolean" null:"NULL" extra:""`
 }
 
 func (c *ModelManager) Users() *UsersModel {
