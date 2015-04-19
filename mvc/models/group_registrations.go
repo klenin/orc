8b1e6e700eca6a9225f2f700ae32cf724545f621
GuraYaroslava <yaroslava_05@mail.ru>
2015-04-19 16:55:49 +1000

    rename groups_registrations.go to group_registrations.go

--- mvc/models/group_registrations.go
+++ mvc/models/group_registrations.go
@@ -1,7 +1,7 @@
 package models
 
 type GroupRegistration struct {
-    Id     int `name:"id" type:"int" null:"NOT NULL" extra:"PRIMARY"`
+    Id      int `name:"id" type:"int" null:"NOT NULL" extra:"PRIMARY"`
     EventId int `name:"event_id" type:"int" null:"NOT NULL" extra:"REFERENCES" refTable:"events" refField:"id" refFieldShow:"name"`
     GroupId int `name:"group_id" type:"int" null:"NOT NULL" extra:"REFERENCES" refTable:"groups" refField:"id" refFieldShow:"name"`
 }
@@ -10,10 +10,10 @@ func (c *ModelManager) GroupRegistrations() *GroupRegistrationModel {
     model := new(GroupRegistrationModel)
 
     model.TableName = "group_registrations"
-    model.Caption = "Групповые регистрации"
+    model.Caption = "Регистрации групп"
 
     model.Columns = []string{"id", "event_id", "group_id"}
-    model.ColNames = []string{"ID", "Лицо", "Мероприятие", "Группа"}
+    model.ColNames = []string{"ID", "Мероприятие", "Группа"}
 
     model.Fields = new(GroupRegistration)
     model.WherePart = make(map[string]interface{}, 0)
