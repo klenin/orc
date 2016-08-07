3e3a47d5e50c0e20e8e5c582c9c172234ea9dec5
Denis <denis_davidyuk@hotmail.com>
2016-08-07 10:54:58 +1000

    blank.js: Fix typo

--- static/js/blank.js
+++ static/js/blank.js
@@ -30,7 +30,7 @@ function(utils, gridLib, datepicker, kladr) {
             block.attr("required", true);
         }
 
-        var lable = $("<label/>", {
+        var label = $("<label/>", {
             text: data["param_name"],
         });
 
@@ -42,7 +42,7 @@ function(utils, gridLib, datepicker, kladr) {
             block.attr("wasChanged", true);
         });
 
-        return $("<p/>").append(lable).append(block);
+        return $("<p/>").append(label).append(block);
     }
 
     function showParam(data, admin) {
@@ -56,14 +56,14 @@ function(utils, gridLib, datepicker, kladr) {
         block.text(data["value"]);
         block.attr("param_val_id", data["param_val_id"]);
 
-        var lable = $("<label/>", {
+        var label = $("<label/>", {
             text: data["param_name"],
             style: "color: #CC6600; font-weight: bold;",
         });
 
         block.attr("readonly", true);
 
-        return $("<p/>").append(lable).append(block);
+        return $("<p/>").append(label).append(block);
     }
 
     function getFormData(id) {
